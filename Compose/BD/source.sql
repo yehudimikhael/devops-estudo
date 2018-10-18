b29593e5fc51f479c510b82b0c8b03320f677fac
Yehudi Mikhael <yehudimikhael@gmail.com>
2018-10-18 06:37:56 -0300

    Correcao do SQL source.sql

--- Compose/BD/source.sql
+++ Compose/BD/source.sql
@@ -1,6 +1,6 @@
 GRANT ALL PRIVILEGES ON inscricao.* TO 'noc'@'%';
-CREATE TABLE IF NOT EXISTS competidores(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), phone VARCHAR(15), vehicle VARCHAR(15))
-INSERT INTO competidores(name, phone, vehicle)values ('Fred', '(21)12345678', 'Hilux')
-INSERT INTO competidores(name, phone, vehicle)values ('Vilma', '(22)9876367', 'L200')
-INSERT INTO competidores(name, phone, vehicle)values ('Carlos', '(23)9993871', 'Ranger')
+CREATE TABLE IF NOT EXISTS competidores(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), phone VARCHAR(15), vehicle VARCHAR(15));
+INSERT INTO competidores(name, phone, vehicle)values('Fred', '(21)12345678', 'Hilux');
+INSERT INTO competidores(name, phone, vehicle)values('Vilma', '(22)9876367', 'L200');
+INSERT INTO competidores(name, phone, vehicle)values('Carlos', '(23)9993871', 'Ranger');
 
