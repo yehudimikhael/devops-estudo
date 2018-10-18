830f900fe8bd688e297b72418807b89a7ba9a4b1
Yehudi Mikhael <yehudimikhael@gmail.com>
2018-10-18 06:20:52 -0300

    Alterações no model.py e source.sql

--- Compose/Back/app/model.py
+++ Compose/Back/app/model.py
@@ -2,36 +2,9 @@ import mysql.connector
 import os
 con = mysql.connector.connect(user='noc', password='concrete123..', host=os.environ.get('DB_HOST'), database='inscricao')
 cur = con.cursor()
-check = 0
-def db_create():
-
-#Se nao existe cria a tabela
-    
-#Criar os dados na tabela
-    try: 
-        cur.execute("CREATE TABLE IF NOT EXISTS competidores(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), phone VARCHAR(15), vehicle VARCHAR(15))")
-        data =  [
-            ('Fred', '(21)12345678', 'Hilux'),
-            ('Vilma', '(22)9876367', 'L200'),
-            ('Carlos', '(23)9993871', 'Ranger')
-            ]
-        cur.executemany("""INSERT INTO competidores
-            (name, phone, vehicle)values (%s, %s, %s)""", data)
-        con.commit()
-        check = 1        
-    except:
-        print("dados existem")
-        check = 1
-        pass
-        return 0
-    con.close()
-    
 
 def db_select():
-    if check == 0:
-        db_create()
-    else:
-        cur.execute("SELECT * FROM competidores")
-        result = cur.fetchall()
-        return result
+    cur.execute("SELECT * FROM competidores")
+    result = cur.fetchall()
+    return result
     con.close()
